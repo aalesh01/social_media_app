f6f9e81ae543eff18881fcdf771a07c266848545
aalesh01 <aalesh111999@gmail.com>
2022-07-26 14:26:19 +0530

    create post done

--- src/components/post /createpost.js
+++ src/components/post /createpost.js
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FormControl,FormLabel,Input,FormHelperText, Button,Heading} from '@chakra-ui/react';
+import './createpost.css'
 
 const CreatePost = () => {
+  
+    const [user,setUser]=useState({"like_count":0});
+    
+    const handleForm= (e) => {
+        const { name, value } = e.target;
+        setUser({ ...user, [name]: value ,})
+    }
+
+    const handleSubmit = async (event) => {
+        console.log(user)
+        await fetch(`http://localhost:8080/posts`, {
+            method: "POST",
+            body: JSON.stringify(user),
+            headers: { "content-type": "application/json" }
+        })
+        alert("Login Successfull");
+    }
+
+
+    
+
+
   return (
-    <div>
- <FormControl>
-  <FormLabel>Email address</FormLabel>
-  <Input type='email' />
-  <FormHelperText>We'll never share your email.</FormHelperText>
+ <div className='postForm'>
+  <Heading>Create a post</Heading>
+ <FormControl colorScheme='blackAlpha' isRequired>
+  <FormLabel>Name</FormLabel>
+  <Input onChange={handleForm} name="name" type='text' />
+  <FormLabel>Image Link</FormLabel>
+  <Input onChange={handleForm} name="image" type='text' />
+  <FormLabel>Write a post</FormLabel>
+  <Input onChange={handleForm} name="text" type='text' />
+  <Button onClick={()=>handleSubmit()} >Post</Button>
 </FormControl>
+
     </div>
   )
 }
