7ecf4b7cae98119edc287bee202019df9b735e65
aalesh01 <aalesh111999@gmail.com>
2022-07-26 22:39:01 +0530

    routes and linking

--- src/App.js
+++ src/App.js
@@ -2,26 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import CreatePost from './components/post /createpost';
 import NavBar from './components/navbar/NavBar';
-import Homepage from './components/home/Homepage';
+import Homepage from './components/home/homepage';
 import {Routes, Route}  from 'react-router-dom';
 import Login from './components/LoginNsignup/login';
 import HomePage from './components/home/homepage';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <>
- <Login/>
-    </>
-=======
     <div className="App">
        <NavBar/> 
       <Routes>
-        <Route path="/login" element={<Login/>}/>
+      <Route path="/login" element={<Login/>}/>
       </Routes>
       <Homepage/>
     </div>
->>>>>>> 6525f8687f941a5c80305191008fb04cc6678fbd
   );
 }
 
--- src/components/home/homepage.js
+++ src/components/home/homepage.js
@@ -1,19 +1,64 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState ,useEffect} from 'react'
+import NavBar from '../navbar/NavBar'
+import Card from './Card'
+import './Homepage.css'
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 
-const HomePage = () => {
-    const [posts, setPosts] = useState([]);
+export default function Homepage() {
+   
+  const [posts, setPosts] = useState([]);
     
  
-   useEffect(()=>{
-        fetch(`http://localhost:8080/posts`)
-        .then (res=>res.json())
-        .then (res=>setPosts(res))
-        console.log(posts);
-   }, []);
+  useEffect(()=>{
+       fetch(`http://localhost:8080/posts`)
+       .then (res=>res.json())
+       .then (res=>setPosts(res))
+       console.log(posts);
+  }, []);
+  
+    const [visible, setVisible] = useState(false)
+    
+    const toggleVisible = () => {
+      const scrolled = document.documentElement.scrollTop;
+      if (scrolled > 300){
+        setVisible(true)
+      } 
+      else if (scrolled <= 300){
+        setVisible(false)
+      }
+    };
+    
+    const scrollToTop = () =>{
+      window.scrollTo({
+        top: 0, 
+        behavior: 'smooth'
+  
+      });
+    };
+    
+    window.addEventListener('scroll', toggleVisible);
+
+
 
   return (
-    <div>H</div>
+    <div>
+    <div className='home-card'>
+      {
+        posts.map(ele=>{
+          <Card img title={ele.name} src ={ele.image} description={ele.text}/>
+        })
+      
+      }
+      
+    </div>
+    <div className='create-post-div'>
+        <button  style={{display: visible ? 'inline' : 'none'}} className='scroll-up'><ArrowUpwardIcon onClick={scrollToTop} 
+       /></button>
+    </div>
+    </div>
+
+
+
+    
   )
 }
-
-export default HomePage
